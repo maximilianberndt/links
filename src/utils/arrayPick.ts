d73e94e3726e33fd5d61e8f3b10869c5b6ce386a
Maximilian Berndt <hallo@maximilianberndt.com>
2022-08-06 18:35:35 +0200

    Fix typing

--- src/utils/arrayPick.ts
+++ src/utils/arrayPick.ts
@@ -1,4 +1,4 @@
-const arrayPick = (array, index = Math.random()) =>
+const arrayPick = <T>(array: Array<T>, index = Math.random()): T =>
   array[Math.floor(index * array.length)];
 
 export { arrayPick };
