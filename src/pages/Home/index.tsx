1e2894b7a12a8e88980878b5d6b62565247b8c7b
Maximilian Berndt <hallo@maximilianberndt.com>
2022-08-07 19:56:00 +0200

    Scroll to top when changing pages

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -17,6 +17,7 @@ const Home = () => {
         itemsPerPage={20}
         startIndex={lastIndex}
         onIndexChange={(index) => {
+          window.scrollTo(0, 0);
           lastIndex = index;
         }}
         items={submissions.map((submission) => (
